a68ecaf5252180cb9300bc13432323c2de197321
muse teshale <musebiranu@gmail.c>
2023-08-29 13:48:33 -0500

    first commit

---
+++ src/main/java/com/example/usermeteadata/UsermeteadataApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermeteadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermeteadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermeteadataApplication.class, args);
+	}
+
+}
